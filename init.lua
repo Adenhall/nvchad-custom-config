d4e8fb345623297f1e5ea7909ce280f1768515f0
Adenhall <jamesadenhall@gmail.com>
2023-07-26 07:53:24 +0700

    feat(eslint_d): stop eslint_d on exit

--- init.lua
+++ init.lua
@@ -1,7 +1,12 @@
--- local autocmd = vim.api.nvim_create_autocmd
+local autocmd = vim.api.nvim_create_autocmd
 
 -- Auto resize panes when resizing nvim window
--- autocmd("VimResized", {
---   pattern = "*",
---   command = "tabdo wincmd =",
--- })
+autocmd("VimResized", {
+  pattern = "*",
+  command = "tabdo wincmd =",
+})
+
+autocmd("VimLeavePre", {
+  pattern = "*",
+  command = "!eslint_d stop"
+})
