a86a606712e9e89f273865403b264668cc81508c
Adenhall <jamesadenhall@gmail.com>
2023-12-03 22:35:38 +0700

    fix: rust debugger could not find lldb-vscode

--- configs/rust-tools.lua
+++ configs/rust-tools.lua
@@ -5,6 +5,14 @@ local options = {
   server = {
     on_attach = on_attach,
     capabilities = capabilities,
+  },
+
+  dap = {
+    adapter = {
+      type = "executable",
+      command = "lldb-vscode-14", -- defaults to lldb-vscode but different on each OS
+      name = "rt_lldb",
+    },
   }
 }
 
